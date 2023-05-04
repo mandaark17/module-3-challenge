31b08a37fcf746e035448742b2ea3bd07cbef60e
mandaark17 <alec.gomez417@gmail.com>
2023-05-04 13:34:18 -0400

    Fixed ceiling and floor of randomIntGen, fixed for loop, ready

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+<img src="./assets/module-3-pic.jpg">
+
 # module-3-challenge
 Password Generator
+
+## Description
+
+- What was your motivation? I went outside of my comfort zone on this one and added random titles to the prompts that will display within the questions
+- This program should provide an almost infinite number of randomly generated passwords for any persons who may need consisntent security generators or something.
+- I learned during this project how to properly first build a function for something before calling it later on. This was important for me during the "Math.Random" portions.
+
+## Usage
+
+I'm sure there are many uses for such a project, however i think sopme of the best cases would be some kind of security program that can use this smaller program to generate quantum passwords in the future?
+
+## Credits
+
+Thank you for taking your time,
+
+Alec Gomez
+
+## License
+MIT License
--- script.js
+++ script.js
@@ -1,53 +1,68 @@
 // Assignment code here
-// Slice and Split string into array to hold all characters
-var charset = 
-`${"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[];:<>,.?/|\\".slice(0, 88)}`.split('');
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppeCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789"; 
+var specialChars = "#$%^&*()_+{}[];:<>,!@.?/|";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
+
+// Develop min/max for Math function
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
 // Gen a password to the writePassword function
 function generatePassword() {
 
-// Min/Max Character Length
-    var length = window.prompt("Ent-ah a numb-uh between 8 and 128", length);
-    var genPassword = "";
+// Min/Max character length
+  var passLength = window.prompt("Ent-ah a numb-uh between 8 and 128", passLength);
+  var genPassword = "";
+  var charPool = ""
 
 // Filtering password criteria
-    if (length <= 7 || length >= 129) {
-        window.alert("Wise guy, huh? Do it again, see what happens.");
-        return;
-            }
-    else{
-        var lower = window.confirm("Need some lowercase lett-ahs?");
-            if (lower === true) {
-                genPassword += charset[0, 26];
-            }
-        var upper = window.confirm("Howza about uppercase lett-ahs?");
-            if (upper === true) {
-                genPassword += charset[27, 52];
-            }
-        var num = window.confirm("Whatza about some numb-uhs?");
-            if (num === true) {
-                genPassword += charset[53, 62];
-            }
-        var special = window.confirm("Oneza dem uh, 'special characters'?");
-            if (special === true) {
-                genPassword += charset[62, 88];
-            }
-        for (var i = 0, i < length, i++) {
-            index = Math.floor(Math.random() * genPassword.length);
-        }
-        return genPassword[index];   
-       }
+  if (passLength <= 7 || passLength >= 129) {
+    window.alert("Wise guy, huh? Do it again, see what happens.");
+    return "";
+  }
+  else {
+    var lower = window.confirm("Need some lowercase lett-ahs?");
+    if (lower === true) {
+      charPool += lowerCaseChars
+    }
+    var upper = window.confirm("Howza about uppercase lett-ahs?");
+    if (upper === true) {
+      charPool += uppeCaseChars
+    }
+    var num = window.confirm("Whatza about some numb-uhs?");
+    if (num === true) {
+      charPool += numbers
+    }
+    var special = window.confirm("Oneza dem uh, 'special characters'?");
+    if (special === true) {
+      charPool += specialChars
+    }
+
+    for (var i = 0; i < passLength; i++) {
+      // generate a random index between 0 and the charPool length
+      randomIndex = getRandomInt(0, charPool.length);
+      // get the charPool character at the randomIndex position
+      var randomChar = charPool[randomIndex];
+      // add the randomChar to the genPassword
+      genPassword += randomChar;
+    }
+    return genPassword;
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
